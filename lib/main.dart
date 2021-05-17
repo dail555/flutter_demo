a1f3245a22676c889f7d10d7e450c7be9be9df79
Wichaivit <wichaivit.pat@gmail.com>
2021-05-17 11:42:47 +0700

    create stateful

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Wichaivit App"),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Wichaivit Pattaramongkolchai",
-                  style: TextStyle(fontSize: 18, color: Colors.blue),
-                ),
-                Text(
-                  "Hello Dart",
-                  style: TextStyle(fontSize: 16, color: Colors.blue),
-                ),
-                Text(
-                  "Hello Flutter",
-                  style: TextStyle(fontSize: 14, color: Colors.blue),
-                ),
-              ],
-            ),
-          )),
+      home: MyHomePage(),
       theme: ThemeData(primarySwatch: Colors.orange),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Wichaivit App"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Wichaivit Pattaramongkolchai",
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                "Hello Dart",
+                style: TextStyle(fontSize: 16, color: Colors.blue),
+              ),
+              Text(
+                "Hello Flutter",
+                style: TextStyle(fontSize: 14, color: Colors.blue),
+              ),
+            ],
+          ),
+        ));
   }
 }
