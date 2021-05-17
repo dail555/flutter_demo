a2c323cd57f33dd40b6ddba81a51d03926ce5397
Wichaivit <wichaivit.pat@gmail.com>
2021-05-17 15:29:59 +0700

    new file FoodMenu.dart & import file & add List<FoodMenu>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'FoodMenu.dart';
 
 void main() {
   print("Starting..");
@@ -23,19 +24,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0;
+  List<FoodMenu> menu = [
+    FoodMenu('กุ้งเผา', '500'),
+    FoodMenu('กะเพราหมู', '80'),
+    FoodMenu('ข้าวผัดไข่', '55'),
+    FoodMenu('ส้มตำ', '40'),
+    FoodMenu('ผัดไท', '60'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Wichaivit App"),
+        title: Text("เมนูอาหาร"),
       ),
-      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title:Text("รายการที่ ${index+1}")
-        );
-      }),
+      body: ListView.builder(
+          itemCount: menu.length,
+          itemBuilder: (BuildContext context, int index) {
+            FoodMenu food = menu[index];
+            return ListTile(
+              title: Text(food.name,style: TextStyle(fontSize:25),),
+              subtitle: Text("ราคา ${food.price} บาท"),
+            );
+          }),
     );
   }
 }
