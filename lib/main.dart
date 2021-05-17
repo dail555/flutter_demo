c2c09ab79be080e25d1297591285ffb0de7d0d4b
Wichaivit <wichaivit.pat@gmail.com>
2021-05-17 09:09:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("Starting..");
+  runApp(MyApp());
+  print("Application is start!");
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Wichaivit App"),
+        ),
+        body: Text("Hello"),
+      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
